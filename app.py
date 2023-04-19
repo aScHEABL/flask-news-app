2b704bbded267da0cc585ef5654d4349cbe6af4c
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-04-19 14:08:24 +0800

    update: comment to zh-tw, news languages in EN

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-# import libraries
+# 匯入相關套件
 from flask import Flask, render_template, request
 from newsapi import NewsApiClient
-  
-# init flask app
+
+# 初始化Flask
 app = Flask(__name__)
   
-# Init news api 
+# 初始化API 
 newsapi = NewsApiClient(api_key='d060d091ddeb4a18af8a7907ce4b88be')
   
-# helper function
+# 主程式
 def get_sources_and_domains():
     all_sources = newsapi.get_sources()['sources']
     sources = []
@@ -35,26 +35,26 @@ def index():
         related_news = newsapi.get_everything(q=keyword,
                                       sources=sources,
                                       domains=domains,
-                                      language='zh',
+                                      language='en',
                                       sort_by='relevancy')
         no_of_articles = related_news['totalResults']
-        if no_of_articles > 50:
-            no_of_articles = 50
+        if no_of_articles > 100:
+            no_of_articles = 100
         all_articles = newsapi.get_everything(q=keyword,
                                       sources=sources,
                                       domains=domains,
-                                      language='zh',
+                                      language='en',
                                       sort_by='relevancy',
                                       page_size = no_of_articles)['articles']
         return render_template("index.html", all_articles = all_articles, 
                                keyword=keyword)
     else:
-        top_headlines = newsapi.get_top_headlines(country="tw", language="zh")
+        top_headlines = newsapi.get_top_headlines(country="tw", language="en")
         total_results = top_headlines['totalResults']
         if total_results > 50:
             total_results = 50
         all_headlines = newsapi.get_top_headlines(country="tw",
-                                                     language="zh", 
+                                                     language="en", 
                                                      page_size=total_results)['articles']
         return render_template("index.html", all_headlines = all_headlines)
     return render_template("index.html")
