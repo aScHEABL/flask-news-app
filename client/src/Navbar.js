85789e3de05a40209071d1e7a34835d8bceae6a7
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-04-25 10:45:08 +0800

    update: make SVG responsive

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Container } from "@chakra-ui/react";
 
 function Navbar() {
-    const logoStyle = {
-        width: '45%',
-      }
-
     return (
-        <Flex as='header' justifyContent='center'>
+        <Container maxW='md'>
             <svg
-            style={logoStyle}    
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
-            width="750"
-            height="100"
+            viewBox="0 0 750 100"
             >
                 <g>
                     <g
@@ -46,7 +40,7 @@ function Navbar() {
                     ></image>
                 </g>
             </svg>
-        </Flex>
+        </Container>
     )
 }
 
