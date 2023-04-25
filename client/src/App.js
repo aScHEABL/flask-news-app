cb92120db9ac34d5dc0d9f2a63432fc7c834dbec
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-04-25 14:02:09 +0800

    update: render headlines and adjust width

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './global.css'
-import { Container } from "@chakra-ui/react";
+import { Container, Box, Center } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 
@@ -12,16 +12,25 @@ function App() {
     .then((response) => response.json()
     .then((data) => {
       setHeadlines(data);
+      console.log(data);
     })
     )
   }, [])
   
-  // const headlinesNodes = headlines.articles.map((news) => <div>{news.title}</div>)
+  const headlinesNodes = headlines.articles.map((news) => <div>{news.title}</div>)
   
   return (
-    <Container>
+    <Container maxW='100ch' centerContent
+    w={{ 
+      sm: '30em', // 480px
+      md: '48em', // 768px
+      lg: '62em', // 992px
+      xl: '80em', // 1280px
+      '2xl': '96em', // 1536px
+     }}>
       <Navbar />
-      {/* {headlinesNodes} */}
+      {headlinesNodes}
+      
     </Container>
   )
 }
