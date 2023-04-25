d9398cd8d966c3c11bc9a65089a779abd1a03c79
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-04-25 10:44:24 +0800

    update: design default page in white first

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import './global.css'
 import { Container } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
@@ -18,7 +19,7 @@ function App() {
   // const headlinesNodes = headlines.articles.map((news) => <div>{news.title}</div>)
   
   return (
-    <Container bgColor='black' minW='100vw' minH='100vh'>
+    <Container>
       <Navbar />
       {/* {headlinesNodes} */}
     </Container>
