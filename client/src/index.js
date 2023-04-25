0248bb97419aa84b08edaddd5dae0f86b706a4e8
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-04-25 10:44:53 +0800

    fix: add ChakraProvider to the root

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
@@ -8,7 +8,9 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
+    <ChakraProvider>
+      <ColorModeScript />
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
