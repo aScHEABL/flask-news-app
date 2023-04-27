9e0550a6157813df1ec08fffcc3152d53d965b29
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-04-27 08:53:54 +0800

    update: change route name

--- server/app.py
+++ server/app.py
@@ -4,19 +4,18 @@ from flask import Flask, render_template
 from newsapi import NewsApiClient
 from flask_cors import CORS
 
-# 初始化Flask
+# 初始化Flask，並且允許Cross-Origin Resource Sharing
 app = Flask(__name__)
 CORS(app)
   
 # 初始化API 
 api = NewsApiClient(api_key='d060d091ddeb4a18af8a7907ce4b88be')
   
-
-@app.route('/defaultView', methods=['GET', 'POST'])
+# 頭條新聞
+@app.route('/headlines', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    # 主程式
-    response = requests.get('https://newsapi.org/v2/top-headlines?country=tw&apiKey=d060d091ddeb4a18af8a7907ce4b88be').json()
+def headlines():
+    response = requests.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=d060d091ddeb4a18af8a7907ce4b88be').json()
     return response
   
 if __name__ == "__main__":
