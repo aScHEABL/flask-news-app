02b21595260c961de6d29beffa5dbe835a4f8aa4
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-04-27 08:54:22 +0800

    update: add card to each news source

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import './global.css'
-import { Container, Box, Center } from "@chakra-ui/react";
+import { Container, Box, CardBody, Flex, Card, CardHeader, Avatar, Heading, Text, IconButton,
+  Icon ,Image, CardFooter, Button } from "@chakra-ui/react";
+// import { BsThreeDotsVertical } from '@chakra-ui/icons';
+import { BiLike, BiChat, BiShare } from 'react-icons/bi';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
+import { newsIcon } from './newsIcon';
+import MainContent from './MainContent';
 
 function App() {
 
-  const [headlines, setHeadlines] = useState();
+  const [headlines, setHeadlines] = useState({articles: []});
 
   useEffect(() => {
-    fetch("/defaultView")
+    fetch("/headlines")
     .then((response) => response.json()
     .then((data) => {
       setHeadlines(data);
@@ -17,10 +23,62 @@ function App() {
     )
   }, [])
   
-  const headlinesNodes = headlines.articles.map((news) => <div>{news.title}</div>)
+  // const headlineNodes = headlines.articles.map((news) => <div>{news.title}</div>)
+  const headlineNodes = headlines.articles.map((news) => 
+        <Card maxW='md'>
+          <CardHeader>
+            <Flex spacing='4'>
+              <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                <Avatar name={news.source.name} src={newsIcon[news.source.name]} />
+
+                <Box>
+                  <Heading size='sm'>{news.source.name}</Heading>
+                  <Text>{news.author}</Text>
+                </Box>
+              </Flex>
+              <IconButton
+                variant='ghost'
+                colorScheme='gray'
+                aria-label='See menu'
+                icon={<BsThreeDotsVertical />}
+              />
+            </Flex>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              {news.description}
+            </Text>
+          </CardBody>
+          <Image
+            objectFit='cover'
+            src={news.urlToImage}
+            alt='新聞連結圖片'
+          />
+
+          <CardFooter
+            justify='space-between'
+            flexWrap='wrap'
+            sx={{
+              '& > button': {
+                minW: '136px',
+              },
+            }}
+          >
+            <Button flex='1' variant='ghost' leftIcon={<BiLike />}>
+              Like
+            </Button>
+            <Button flex='1' variant='ghost' leftIcon={<BiChat />}>
+              Comment
+            </Button>
+            <Button flex='1' variant='ghost' leftIcon={<BiShare />}>
+              Share
+            </Button>
+          </CardFooter>
+        </Card>
+  )
   
   return (
-    <Container maxW='100ch' centerContent
+    <Container maxW='120ch' centerContent
     w={{ 
       sm: '30em', // 480px
       md: '48em', // 768px
@@ -29,8 +87,10 @@ function App() {
       '2xl': '96em', // 1536px
      }}>
       <Navbar />
-      {headlinesNodes}
-      
+      <Flex wrap='wrap' gap='6' justify='center'>
+        { headlineNodes }
+      </Flex>
+      {/* <MainContent data={headlines} /> */}
     </Container>
   )
 }
