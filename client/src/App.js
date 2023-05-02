bffc7a46bd905f08573131027502dcfd701a0c5a
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-05-02 08:11:06 +0800

    update: default routing to general

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './global.css'
+import { Helmet } from "react-helmet";
 import { Container } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "./Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import General from './components/General';
 import Science from './components/Science';
 import Tech from './components/Tech';
@@ -22,9 +23,12 @@ function App() {
       xl: '80em', // 1280px
       '2xl': '96em', // 1536px
      }}>
+      {/* <Helmet>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+      </Helmet> */}
       <Navbar />
       <Routes>
-        <Route path="/" element={<General />} />
+        <Route path="/" element={<Navigate to="/general" />} />
         <Route path="/general" element={<General />} />
         <Route path='/science' element={<Science />} />
         <Route path='/technology' element={<Tech />} />
@@ -32,6 +36,12 @@ function App() {
         <Route path='/business' element={<Business />} />
         <Route path='/entertainment' element={<Entertainment />} />
         <Route path='/health' element={<Health />} />
+        <Route path="*" element={
+              <div>
+                <h2>404 Page not found</h2>
+              </div>
+            }
+          />
       </Routes>
       
     </Container>
